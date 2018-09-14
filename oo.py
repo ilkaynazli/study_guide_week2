b6e1085878482c4af0d6111d30af23313ed919b2
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-14 16:44:10 -0700

    classes part 3 done

--- oo.py
+++ oo.py
@@ -1,4 +1,4 @@
-class Student():
+class Student:
     """A class for students with name and address"""
 
     def __init__(self, f_name, l_name, address):
@@ -7,7 +7,7 @@ class Student():
         self.address = address
 
 
-class Question():
+class Question:
     """A class for questions with correct answers"""
 
     def __init__(self,question,correct_a):
@@ -20,12 +20,12 @@ class Question():
                 
 
 
-class Exam():
+class Exam:
     """A class for exams with questions"""
 
-    def __init__(self, name, questions=[]):
+    def __init__(self, name):
         self.name = name
-        self.questions = questions
+        self.questions = []
 
     def add_question(self, question):
         self.questions.append(question)
@@ -39,3 +39,28 @@ class Exam():
         return (correct_count/len(self.questions))* 100
 
 
+class StudentExam:
+    """A class that store student, exam and score"""
+
+    def __init__(self, student, exam):
+        self.exam = exam
+        self.student = student
+
+    def take_test(self):
+        self.score = self.exam.administer()
+        print("Student's score is {}".format(self.score))
+
+
+def example():
+    exam = Exam('Midterm')
+    set_q = Question('What is the method for adding an element to a set?','.add()')
+    exam.add_question(set_q)
+    pwd_q = Question('What does pwd stand for?','print working directory')
+    exam.add_question(pwd_q)
+    list_q = Question('Python lists are mutable, iterable, and what?','ordered')
+    exam.add_question(list_q)
+    student1 = Student("Jasmine", "Debugger", "0101 Computer Street")
+    studentExam1 = StudentExam(student1, exam)
+    studentExam1.take_test()
+
+example()
